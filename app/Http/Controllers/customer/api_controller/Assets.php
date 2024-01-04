6a44b4b5d62b8e02aa08faceba3ad6624305ab53
meettan <meettan@synergicsoftek.com>
2024-01-04 23:14:48 +0530

    test

--- app/Http/Controllers/customer/api_controller/Assets.php
+++ app/Http/Controllers/customer/api_controller/Assets.php
@@ -145,10 +145,10 @@ class Assets extends ResponceBaseController
         }
     }
 
-    function list_product_mastar(): JsonResponse
+    function list_product_mastar()
     {
         try {
-            $user_type=auth()->user()->type;
+            return $user_type=auth()->user()->type;
             $data = MdProduct::join("md_unit as a", "a.unit_id", "=", "md_product.unit_id")
                 //->join("users as b","b.id","=","md_product.")
                 ->where('md_product.user_type',$this->getUserType($user_type))
