7e366a6796518e36561614b31b8e7bd5814eab81
meettan <meettan@synergicsoftek.com>
2024-01-15 21:11:54 +0530

    abc

--- app/Http/Controllers/customer/api_controller/Label2Action.php
+++ app/Http/Controllers/customer/api_controller/Label2Action.php
@@ -8,6 +8,7 @@ use App\Models\TdLabel1;
 use App\Models\TdLabel2;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class Label2Action extends ResponceBaseController
@@ -17,7 +18,7 @@ class Label2Action extends ResponceBaseController
         try {
             $rules = [
                 'l2_qty' => 'required|integer',
-                'product_mastar_id' => 'required|integer',
+                'batch_no' => 'required|integer',
             ];
             $valaditor = Validator::make($r->all(), $rules);
             if ($valaditor->fails()) {
@@ -30,7 +31,7 @@ class Label2Action extends ResponceBaseController
             $data = TdLabel2::create([
                 "create_by" => auth()->user()->id,
                 "l2_qty" => $r->l2_qty,
-                "product_mastar_id" => $r->product_mastar_id,
+                "batch_no" => $r->batch_no,
                 "l2_stock" => "A",
                 "l2_flag" => "A",
                 "update_by" => auth()->user()->id
@@ -49,8 +50,7 @@ class Label2Action extends ResponceBaseController
         try {
             $rules = [
                 'l2_qty' => 'required|integer',
-                'label2_id' => 'required|integer',
-                'product_mastar_id' => 'required|integer',
+                'batch_no' => 'required|integer',
             ];
             $valaditor = Validator::make($r->all(), $rules);
             if ($valaditor->fails()) {
@@ -88,15 +88,46 @@ class Label2Action extends ResponceBaseController
 
     function work_list():JsonResponse
     {
+        // try {
+        //     $data = TdLabel1::join("md_master_product as a", 'a.product_id', '=', 'td_label1.product_mastar_id')
+        //         ->join("md_unit as b", 'b.unit_id', '=', 'a.unit_id')
+        //         ->where("td_label1.update_by", auth()->user()->id)
+        //         ->where("td_label1.l1_stock", "A")
+        //         ->where("td_label1.l1_flag", "A")
+        //         ->select("td_label1.*", "a.product_name", "b.*")
+        //         ->groupBy("td_label1.batch_no","td_label1.product_mastar_id")
+        //         ->get();
+
+        //     return $this->sendResponse($data, " ");
+        // } catch (\Throwable $th) {
+        //     return $this->sendError("exception handler error", $th, 400);
+        // }
+
+
         try {
-            $data = TdLabel1::join("md_master_product as a", 'a.product_id', '=', 'td_label1.product_mastar_id')
-                ->join("md_unit as b", 'b.unit_id', '=', 'a.unit_id')
-                ->where("td_label1.update_by", auth()->user()->id)
-                ->where("td_label1.l1_stock", "A")
-                ->where("td_label1.l1_flag", "A")
-                ->select("td_label1.*", "a.product_name", "b.*")
-                ->groupBy("td_label1.batch_no","td_label1.product_mastar_id")
-                ->get();
+            $data = TdLabel1::join("md_product as a", 'a.product_id', '=', 'td_label1.product_id')
+            ->join("md_unit as b", 'b.unit_id', '=', 'a.unit_id')
+            ->join("md_master_product as c", 'td_label1.product_mastar_id', '=', 'c.id_master_product')
+            // ->where("td_label1.update_by", auth()->user()->id)
+            ->where("td_label1.l1_stock", "A")->where("td_label1.l1_flag", "A")
+            ->select(
+                "td_label1.batch_no",
+                DB::raw("MAX(td_label1.label1_id) as label1_id"),
+                DB::raw("MAX(td_label1.l1_qty) as l1_qty"),
+                "td_label1.product_mastar_id",
+                "td_label1.product_id",
+                DB::raw("MAX(td_label1.l1_stock) as l1_stock"),
+                DB::raw("MAX(td_label1.l1_flag) as l1_flag"),
+                DB::raw("MAX(td_label1.type) as type"),
+                "b.unit_id",
+                "b.unit_name",
+                "b.unit_size",
+                "a.product_name",
+                DB::raw("MAX(a.qty) as qty"),
+                "c.product_name as mastar_product_name"
+            )
+            ->groupBy("td_label1.batch_no", "td_label1.product_mastar_id", "td_label1.product_id", "b.unit_id", "b.unit_name", "b.unit_size", "a.product_name","c.product_name")
+            ->get();
 
             return $this->sendResponse($data, " ");
         } catch (\Throwable $th) {
